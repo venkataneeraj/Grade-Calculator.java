62187ad890264668a5443b650ae9c095d2162ba6
venkataneeraj <67872311+venkataneeraj@users.noreply.github.com>
2020-08-10 23:23:30 +0530

    Created Grade Calculator for java
    This will help you to calculate grades of students without any problems

---
+++ Grade Calculator.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grade.calculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author NEERAJ
+ */
+public class GradeCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+        
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter average of your marks (less than 100)::");
+      int average = sc.nextInt();
+      char grade;
+
+      if(average>=80){
+         grade = 'A';
+      }else if(average>=60 && average<80){
+         grade = 'B';
+      }
+      else if(average>=40 && average<60){
+         grade = 'C';
+      }
+      else {
+         grade = 'D';
+      }
+
+      switch(grade) {
+         case 'A' :
+            System.out.println("Excellent!");
+            break;
+         case 'B' :
+         case 'C' :
+            System.out.println("Well done");
+            break;
+         case 'D' :
+            System.out.println("You passed");
+         case 'F' :
+            System.out.println("Better try again");
+            break;
+         default :
+         System.out.println("Invalid grade");
+      }
+      System.out.println("Your grade is " + grade);
+   }
+}
+    
+    
